70030cfd4a91c3b08df8d38f142033f5cb5f0ae8
vumg <ab17100222@gmail.com>
2019-10-24 12:26:18 -0500

    segundo ejercicio de Write

--- src/Streams/src/com/streams2/app/WritterApp.java
+++ src/Streams/src/com/streams2/app/WritterApp.java
@@ -1,27 +1,46 @@
 package com.streams2.app;
 
-import java.io.BufferedReader;
+
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class WritterApp {
 	public static void main(String[] dasd) {
+		
+		//ejercicio: pedir una tabla al usuario y escribir en un archivo esa tabla hasta el numero 24
+		
+		
+		//constantes
+		int LIMIT = 24;
 		
 		File myFile = null;
 		FileWriter myWriter = null;
 		BufferedWriter myBuffer = null;
 		
+		//nueva variable para mostrar las tablas
+		int tabla = 0;
+		
+		//declaracion de objeto scanner
+		Scanner input = new Scanner(System.in);
+		
 		
 		
 		try {
 			
-			myFile = new File("c:\\carpeta\\nota.txt");
+			myFile = new File("c:\\carpeta\\c.txt");
 			myWriter = new FileWriter(myFile);
 			myBuffer = new BufferedWriter(myWriter);
-			myBuffer.write("Este es el comienzo de la historia, que gran momento de estar vivos...");
+			
+			//leemos la tabla
+			tabla = input.nextInt();
+			for(int i = 0; i<LIMIT; i++) {
+				System.out.println(tabla+" * "+i+" = "+i*tabla);
+				myBuffer.write(String.format("%d X %d = %d %n", tabla,i,tabla*i));
+				
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
