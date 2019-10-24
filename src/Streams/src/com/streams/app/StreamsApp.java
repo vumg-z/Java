30f92ca1d9460beb6424740f38e0c633090158cd
vumg <ab17100222@gmail.com>
2019-10-24 11:50:38 -0500

    Primer practica de Streams terminada

--- src/Streams/src/com/streams/app/StreamsApp.java
+++ src/Streams/src/com/streams/app/StreamsApp.java
@@ -1,5 +1,43 @@
 package com.streams.app;
+import java.io.File;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 
 public class StreamsApp {
-
+	public static void main(String[] arasd) {
+		
+		File myFile = null;
+		FileReader myReader = null;
+		BufferedReader myBuffer = null;
+		
+		String line = "";
+		
+		try {
+			
+			myFile = new File("c:\\carpeta\\nota.txt");
+			myReader = new FileReader(myFile);
+			myBuffer = new BufferedReader(myReader);
+			
+			
+			
+			//imprimir todas las lineas
+			while((line = myBuffer.readLine()) != null) {
+				System.out.println(line);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			try {
+				myBuffer.close();
+				myReader = null;
+				myFile = null;
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
+	}
 }
