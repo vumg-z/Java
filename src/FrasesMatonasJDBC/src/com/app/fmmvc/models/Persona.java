012ea0ef114155d178cf4093c0e19f9fb365dab0
vumg <ab17100222@gmail.com>
2019-11-19 18:47:11 -0600

    agregue los modelos

--- src/FrasesMatonasJDBC/src/com/app/fmmvc/models/Persona.java
+++ src/FrasesMatonasJDBC/src/com/app/fmmvc/models/Persona.java
@@ -1,5 +1,9 @@
 package com.app.fmmvc.models;
 
 public class Persona {
-
+	public int idPersona = 0;
+	public String nombre = "";
+	public String edad = "";
+	public String carrera = "";
+	public String fecha = "";
 }
